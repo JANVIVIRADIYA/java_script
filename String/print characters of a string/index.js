6b3ae21336ab4e973bd96267fad9cfcefff6d38f
JANVIVIRADIYA <viradiyajanvi1104@gmail.com>
2024-04-03 18:34:38 +0530

    string

--- String/print characters of a string/index.js
+++ String/print characters of a string/index.js
@@ -1,11 +1,7 @@
-const name = "rednwhite";
-
-console.log(name[0]);
-console.log(name[1]);
-console.log(name[2]);
-console.log(name[3]);
-console.log(name[4]);
-console.log(name[5]);
-console.log(name[6]);
-console.log(name[7]);
-console.log(name[8]);
+function result(){
+    var name = document.getElementById("name").value;{
+        for(var i=0;i<name.length;i++){
+            console.log(name[i])
+        }
+    }
+   }
